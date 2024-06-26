0c3a4621d3a8632517b02f3a9712024ffc6b6e7f
aloysius@frogman <122674536+Frogman46@users.noreply.github.com>
2024-06-26 18:58:12 +0530

    Delete Day 1 directory

--- Day 1/Day 1 Basic.py
+++
@@ -1,3 +0,0 @@
-print("Hello World")
-input("What is my name?\n")
-print("Hope you are doing well")
--- Day 1/Day 1 New Line Input Space.py
+++
@@ -1,7 +0,0 @@
-print("Hello World!\nHello World!\nHello World!\n")
-print("Hello" +" " + "Aloysius")
-input("what is your name?\n")
-print("Hello " + input("what is your name? "))
-print(len(input("what is your name? ")))
-
-

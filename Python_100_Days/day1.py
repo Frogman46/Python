ff8a5dc8e6871144599aa7fe8e226e67133444a0
aloysius@frogman <122674536+Frogman46@users.noreply.github.com>
2024-06-26 20:50:02 +0530

    Day one completed

---
+++ Python_100_Days/day1.py
@@ -0,0 +1,65 @@
+print("Hello world!")
+# Here you have the key world print followed by the set of prentices in which tell it what you want it to print - print("something") and once you have inserted that and when the line of code is executed by the computer, it'll know to simply print or output the thing that you've have placed between the parentheses.
+# Notice that there is double quotation marks, and this denotes to the computer that the bit between the quotation is not a code rather it is just a simple text that I have made up and this pieces of text in programming language is called Strings
+# Strings - It is the strings of character and what the double quotes does is that it will the beginning and the end of the strings of characters.
+
+# 1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.
+# 2. Knead the dough for 10 minutes.
+# 3. Add 3g of Salt.
+# 4. Leave to rise for 2 hours.
+# 5. Bake at 200 degrees C for 30 minutes.
+
+# Print the above the same way as it is stated above.
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+print('She said: "Hello" and then left.')
+print("She said: \"Hello\" and then left.")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("Hello world!\nHello world!\nHello world!")
+# In the above line \n denotes a new line and in this way you can save time
+
+print("Hello" + "AJ")
+# The above line is called concatenate. What we see is that + sign is added to denote one word, and when you run it it will show as HelloAJ as it does not have any sign stating space.
+# There are three ways to add space. 1) Add a space after "Hello " 2) Add a space before " AJ" and 3) is "Hello" + " " + "AJ"
+
+print("Hello " + "AJ")
+print("Hello" + " AJ")
+print("Hello" + " " + "AJ")
+
+# In python it is important to remember to not sue any space ot tab before starting to write a new line of code or you will get a unexpected indent error
+
+# Fix the code below 👇
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#  print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")     
+
+# Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+#Input
+# input()function
+input("A prompt for the user")
+
+print("Hello " + input("What is your name?"))
+
+# In the above code we can see that 1 there is hello and then we concatenate it with the input "what is your name" and below in the terminal we can add the prompt and see the result
+
+
+
+
+
